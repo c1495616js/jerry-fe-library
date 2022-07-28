752dad6a06ebce04e09205501944cb14234b81b0
Jerry Wang <jerryappleid761208@gmail.com>
2022-07-28 10:12:53 -0700

    add stylelint

--- packages/browserslist-config/index.js
+++ packages/browserslist-config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: [
-    "last 1 chrome version",
-    "last 1 firefox version",
-    "last 1 safari version",
+    'last 1 chrome version',
+    'last 1 firefox version',
+    'last 1 safari version',
   ],
-  production: [">0.25%", "last 2 version", "not dead", "not ie <= 11"],
+  production: ['>0.25%', 'last 2 version', 'not dead', 'not ie <= 11'],
 };
