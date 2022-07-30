03598f3a9e57ad54acdb13e9468fcd48c4453cf3
Jerry Wang <jerryappleid761208@gmail.com>
2022-07-30 10:48:24 -0700

    feat: add new type

--- packages/type-utils/src/index.ts
+++ packages/type-utils/src/index.ts
@@ -1,1 +1,2 @@
 export * from './aliases-and-guards';
+export * from './mapped-types';
---
+++ packages/type-utils/src/mapped-types.ts
@@ -0,0 +1,18 @@
+import { Primitive } from './aliases-and-guards';
+
+/**
+ * Credits to all the people who given inspiration and shared some very useful code snippets
+ * in the following github issue: https://github.com/Microsoft/TypeScript/issues/12215
+ */
+
+/**
+ * SetIntersection (same as Extract)
+ * @desc Set intersection of given union types `A` and `B`
+ * @example
+ *   // Expect: "2" | "3"
+ *   SetIntersection<'1' | '2' | '3', '2' | '3' | '4'>;
+ *
+ *   // Expect: () => void
+ *   SetIntersection<string | number | (() => void), Function>;
+ */
+export type SetIntersection<A, B> = A extends B ? A : never;
